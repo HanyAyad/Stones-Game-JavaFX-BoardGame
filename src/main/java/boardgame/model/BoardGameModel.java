35a4abe64d10dba2495679b275ea524efd3794ce
Eskandar Hany Ayad <hanyayad71@gmail.com>
2023-05-15 17:45:17 +0200

    Defined an instance of the GameResults class to store the game information. Declared game data variables including: player1/2 names, winner name etc... created a setter method for player/winner names that is called in the BoardGameController class. Also created a method to save game record after the end of each game in a list inside a json file

--- src/main/java/boardgame/model/BoardGameModel.java
+++ src/main/java/boardgame/model/BoardGameModel.java
@@ -1,17 +1,29 @@
 package boardgame.model;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoardGameModel {
 
+    private GameResults gameResult;
     public static final int BOARD_SIZE = 3;
-
     public ReadOnlyObjectWrapper<Square>[][] board = new ReadOnlyObjectWrapper[BOARD_SIZE][BOARD_SIZE];
-
     private Player currentPlayer = Player.PLAYER1;
     private boolean gameOver = false;
     private Player winner = null;
+
+    private String player1Name;
+    private String player2Name;
+
+    private String winnerName;
 
 
     public BoardGameModel() {
@@ -20,6 +32,8 @@ public class BoardGameModel {
                 board[i][j] = new ReadOnlyObjectWrapper<Square>(Square.NONE);
             }
         }
+        gameResult = new GameResults();
+        gameResult.setGameStartTime(LocalDateTime.now().toString());
     }
 
     public ReadOnlyObjectProperty<Square> squareProperty(int i, int j) {
@@ -40,6 +54,13 @@ public class BoardGameModel {
                         case GREEN -> Square.NONE;
                     }
             );
+
+            if(currentPlayer == Player.PLAYER1){
+                gameResult.setPlayer1Turns(gameResult.getPlayer1Turns()+1);
+            }
+            else{
+                gameResult.setPlayer2Turns(gameResult.getPlayer2Turns()+1);
+            }
             checkWinner();
             switchPlayers();}
 
@@ -133,10 +154,54 @@ public class BoardGameModel {
     }
 
     public void resetGame(){
+        if (winner!=null){saveGameResult();}
         currentPlayer=Player.PLAYER1;
         gameOver=false;
         winner=null;
+        gameResult.setWinnerName(null);
+        gameResult.setPlayer2Turns(0);
+        gameResult.setPlayer1Turns(0);
+        gameResult.setGameStartTime(LocalDateTime.now().toString());
+
+
     }
+    public void setPlayerNames(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
+        gameResult.setPlayer1Name(player1Name);
+        gameResult.setPlayer2Name(player2Name);
+    }
+    public void setWinnerName(String winnerName) {
+        this.winnerName = winnerName;
+        gameResult.setWinnerName(winnerName);
+    }
+
+    private void saveGameResult() {
+        ObjectMapper mapper = new ObjectMapper();
+        File file = new File("game_results.json");
+
+        // Read existing game results from the file, if any
+        List<GameResults> gameResultsList = new ArrayList<>();
+        if (file.exists()) {
+            try {
+                gameResultsList = mapper.readValue(file, new TypeReference<List<GameResults>>() {});
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Add the current game result to the list
+        gameResultsList.add(gameResult);
+
+        // Write the updated list back to the file
+        try {
+            mapper.writeValue(file, gameResultsList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
 
 
